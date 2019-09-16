6dbd8f0c9ae8a6a9825fc9885237384a59026fb8
JGCardelus <51745043+JGCardelus@users.noreply.github.com>
2019-09-16 10:13:40 +0200

    Add table creating for already indexed files.

---
+++ indexManager.py
@@ -0,0 +1,49 @@
+import os
+import json
+
+class IndexManager:
+    def __init__(self):
+        self.index = json.load(open('index.txt', 'r'))
+        self.home_path = os.getcwd()
+        self.actual_cell = None
+
+        self.name = None
+        self.dir_ = None
+        self.files = None
+
+    def save(self):
+        print("Saving index database")
+        os.chdir(self.home_path) #Return to home path
+        with open('index.txt', 'w') as output:
+            json.dump(self.index, output)
+
+    def define_folder(self, name, dir_):
+        self.name = name
+        self.dir_ = dir_
+        self.files = []
+
+    def create_folder(self, parent_folders):
+        if self.actual_cell == None:
+            self.actual_cell = self.index["folders"]
+
+        cell = self.select_cell(self.actual_cell, parent_folders)
+        cell.append({"name": self.name, "dir": self.dir_, "files": self.files, "folders": []})
+
+        self.actual_cell = cell
+
+        #print(self.index)
+
+    def select_cell(self, starting_cell, parent_folders):
+        cell = self.index["folders"]
+        #If we are not in the wanted cell
+        if parent_folders != []:
+            for i, parent_folder in enumerate(parent_folders): #Go through the parent folders
+                for folder_cell in cell: #Go trough the folders inside the folder we're searching
+                    print(folder_cell)
+                    if folder_cell["name"] == parent_folder: #If the folder we are looking for is the atual one
+                        #if i < (len(parent_folders) - 1):
+                        cell = folder_cell['folders'] #Select folder
+        print(cell)
+        return cell
+
+index_manager = IndexManager()
--- indexator.py
+++ indexator.py
@@ -2,6 +2,8 @@ import libs
 import cleaner
 import os
 
+from indexManager import index_manager
+
 class DirectoryIndexator:
     def __init__(self, root, remove_plural=True, exclude=[]):
         self.root = root
@@ -19,11 +21,17 @@ class DirectoryIndexator:
             root_path = root.split('\\')
             root_name = root_path[len(root_path) - 1]
 
+            #Define branch in index manager
+            index_manager.define_folder(root_name, root)
+
             #Search directory for folders and files
             for root, branches, files in os.walk('.'):
                 #Change file names in root folder
                 for file_name in files:
                     self.rename_file([root_name], file_name)
+
+                #Save branch
+                index_manager.create_folder([])
 
                 #Check that the directory name is not in the exlude parameter
                 for branch in branches:
@@ -31,16 +39,31 @@ class DirectoryIndexator:
                         #Directory must be included in reindexation
                         self.search_directory([root_name, branch])
 
+            #Save index
+            index_manager.save()
+
     def search_directory(self, tree):
         #Go to the folder
         path = self.root + '\\' + '\\'.join(tree[1:]) #Since tree has root_name at the begining, exclude as it is also contained in self.root
         libs.go(path)
+
+        #Define branch in index manager
+        index_manager.define_folder(tree[len(tree) - 1], path)
 
         #Search branch
         for root, branches, files in os.walk('.'):
             #Rename files
             for file_name in files:
                 self.rename_file(tree, file_name)
+
+            #Save brach in index manager
+            #parent_folders = []
+            #If last cell == last folder indexed, no need to find location again
+            #if index_manager.actual_cell["name"] != tree[len(tree) - 2]: 
+                #If is not, search for folder again
+            parent_folders = tree[:len(tree) - 1]
+
+            index_manager.create_folder(parent_folders)
 
             #If there are branches in the actual branch apply transformation
             for branch in branches:
@@ -51,11 +74,30 @@ class DirectoryIndexator:
                     self.search_directory(updated_tree)
 
     def rename_file(self, raw_tree, raw_file_name):
+        #File might be already indexed
+        raw_chunks = raw_file_name.split('_')
+        original_file_name = raw_chunks[len(raw_chunks) - 1] #prefix_prefix_name.extension
+        
         #Clean tree names
         tree = cleaner.clean_tree(raw_tree, self.remove_plural)
         file_name = cleaner.clean_name(raw_file_name)
         prefix = '_'.join(tree)
+        #If file is not indexed, this is name to savee
         name_ = prefix + '_' + file_name
-        
-        os.rename(raw_file_name, name_)
 
+        #If file is already indexed, this is target name
+        target_name = prefix + '_' + original_file_name
+        
+        if target_name != raw_file_name:
+            #It hasn't been indexed
+            os.rename(raw_file_name, name_)
+            #Save name in index_manager
+            index_manager.files.append(name_)
+        else:
+            #It has been indexed
+            #Save name in index_manager
+            index_manager.files.append(raw_file_name)
+
+home_dir = "C:\\Users\\jgcar\\Desktop\\idxTest"
+indexator = DirectoryIndexator(home_dir)
+indexator.start()
